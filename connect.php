1bd59de9c97f24b88d03a87f2e15330cf45352ae
kamikazze05 <150346933+kamikazze05@users.noreply.github.com>
2025-03-25 22:32:10 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$db="login";
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error){
+    echo "Failed To Connect DB".$conn->connect_error;
+}
+?>
